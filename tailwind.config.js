b3a57150e308c522c9600fc2b1bf258355b015d8
farazpc3 <farazpc3@gmail.com>
2025-06-01 14:35:38 +0330

    Initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.tsx", "./pages/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        vazirmatn: ["Vazirmatn", "sans-serif"],
+      },
+    },
+  },
+};
